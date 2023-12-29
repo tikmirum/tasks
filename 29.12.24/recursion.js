e27437c5b2b730f80e9ea650f205abf0766a64be
Tigran Mirumyan <tikmirumian@mail.ru>
2023-12-29 12:16:04 +0400

    Add:recursion 3 method

--- 29.12.24/recursion.js
+++ 29.12.24/recursion.js
@@ -26,3 +26,27 @@ function recursion1(params) {
 }
 
 recursion1(5)
+
+let sum = 1;
+let finish = 1;
+let counter = 1;
+function  recursion2(params) {
+    if(params < 1){
+        return "number must be greater than 1"
+    }
+    if(params === 1){
+        return 1;
+    };
+    if(sum < params){
+
+        sum++
+        finish = sum * recursion2(params);
+        if(counter === sum){
+            finish,counter,sum = 1;
+        }
+        return finish;
+    }
+    return finish;
+}
+
+recursion2(5)
