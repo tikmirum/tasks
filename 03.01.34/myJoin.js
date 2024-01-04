2e22ff376f793d953cf02e20e694f380a287e45e
Tigran Mirumyan <tikmirumian@mail.ru>
2024-01-04 11:04:31 +0400

    add:myJoin array method

--- 03.01.34/myJoin.js
+++ 03.01.34/myJoin.js
@@ -1,14 +1,23 @@
 const arr =[10,20,30,40,50]
-Array.prototype.myJoin = function(name) {
-    const newArr=[];
-    for(let i=0; i<this.length; i++){
-        newArr.push(i);
-        newArr.push(name);
+arr.myJoin = function(name) {
+    if(typeof name !== 'string'){
+        console.log('argument must be a string')
+        return
     }
-    return newArr;
+    let newName = name.toString();
+    let newString = '';
+    for(let i=0; i<this.length; i++){
+        const el = this[i];
+        if(i === this.length - 1){
+            newName = "";
+        }
+        const bull = `${el}${newName}`;
+        newString += bull
+    }
+    return newString;
 };
 
 
-const myFoo = arr.myJoin('+');
+const myFoo = arr.myJoin(' ');
 
 console.log(myFoo)
